03419f3f072d93758198b264bd1b360028c7cbbb
Nikita Sinha <nikita@Nikitas-MacBook-Pro.local>
2019-05-14 10:27:36 +0530

    Connected to database

--- routes/store.js
+++ routes/store.js
@@ -3,10 +3,8 @@ var router = express.Router();
 var pg=require('pg');
 var pgp = require('pg-promise')();
 const { Client } = require('pg')
-router.get('/',function(req, result, next) 
+router.get('/', async(req, result, next) =>
 {
-
-  console.log('nikita here' ,req);
   var random_in=req.query.random_in;
   const { Pool, Client } = require('pg')
   const pool = new Pool
@@ -15,25 +13,35 @@ router.get('/',function(req, result, next)
   host: 'ec2-23-23-184-76.compute-1.amazonaws.com',
   database: 'd5kfa12hcv1faj',
   password: 'b70e2f501af3e750b72ef65948da3cfebf9c64c210dd25fa93073cb3fbb774fa',
-  port: 5432,
+  port:5432
 })
   const client = new Client({
   user: 'roryqgrrvgjrjl',
   host: 'ec2-23-23-184-76.compute-1.amazonaws.com',
   database: 'd5kfa12hcv1faj',
   password: 'b70e2f501af3e750b72ef65948da3cfebf9c64c210dd25fa93073cb3fbb774fa',
-  port:5432,
+  port:5432
 })
 var d = new Date();
- client.connect()
- client.query('INSERT into Bibo_table (percentage_in, percentage_out,correct_in,correct_out,time) values ($1, $2, $3, $4,$5)',[percentage_in, percentage_out,correct_in,correct_out,d], (err, res) => 
+
+ await client.connect()
+ console.log('request :' , req.query, req.query.percentage_in);
+ var percentage_in = req.query.percentage_in;
+ var percentage_out = req.query.percentage_out;
+ var correct_in = req.query.correct_in;
+ var correct_out = req.query.correct_out;
+ await client.query('INSERT into Bibo_table (percentage_in, percentage_out,correct_in,correct_out,created_at) values ($1, $2, $3 ,$4,$5)',[percentage_in, percentage_out,correct_in,correct_out,d], (err, res) => 
  {
+   console.log('request sent')
     if (err) 
     {
       console.log('faill')
       console.log(err.stack);
     };
-    return result.send('success')
+    if(res) {
+      console.log('res', res)
+      return result.send('success')
+    }
  });
   console.log(random_in)
 }
@@ -47,7 +55,7 @@ router.get('/getitem',  async(req,res,next)=>
   host: 'ec2-23-23-184-76.compute-1.amazonaws.com',
   database: 'd5kfa12hcv1faj',
   password: 'b70e2f501af3e750b72ef65948da3cfebf9c64c210dd25fa93073cb3fbb774fa',
-  port: 5432,
+  port:5432,
  });
  const client = new Client(
   {
@@ -59,7 +67,7 @@ router.get('/getitem',  async(req,res,next)=>
   })
 await client.connect()
 data = {}
-const result = await client.query('SELECT percentage_in,percentage_out,correct_in,correct_out from Bibo_table ORDER BY time DESC')
+const result = await client.query('SELECT percentage_in,percentage_out,correct_in,correct_out from Bibo_table ORDER BY created_at DESC')
 console.log(result);
 dbQuery1 =  await client.query('SELECT COUNT(*) FROM Bibo_table');
 dbQuery2=  await client.query('SELECT ROUND(AVG(percentage_in) ,0) FROM Bibo_table');
@@ -72,7 +80,7 @@ data['breathe_out_percent']=dbQuery3['rows'][0].round;
 data['breathe_in_correct']=dbQuery4['rows'][0].round;
 data['breathe_out_correct']=dbQuery5['rows'][0].round;
 data['previous_result'] = result ['rows'][0];
-res.send('HI');
+res.send(data);
 // res.send(result ['rows'][0]);
 await client.end()
 });
